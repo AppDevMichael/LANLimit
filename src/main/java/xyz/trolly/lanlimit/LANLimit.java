6deb989742ed4cdc137b835ac6add9a1d995f802
Michael O'Connell <33251702+AppDevMichael@users.noreply.github.com>
2022-01-03 22:46:59 +0000

    Fixed Mixins

--- src/main/java/xyz/trolly/lanlimit/LANLimit.java
+++ src/main/java/xyz/trolly/lanlimit/LANLimit.java
@@ -3,7 +3,6 @@ package xyz.trolly.lanlimit;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 public class LANLimit implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
