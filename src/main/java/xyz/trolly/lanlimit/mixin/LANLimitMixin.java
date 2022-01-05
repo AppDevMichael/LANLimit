569ccaf75a7237011d38ccad49516512f616ddd9
Michael O'Connell <33251702+AppDevMichael@users.noreply.github.com>
2022-01-05 00:21:13 +0000

    Fixed the mixin!

--- src/main/java/xyz/trolly/lanlimit/mixin/LANLimitMixin.java
+++ src/main/java/xyz/trolly/lanlimit/mixin/LANLimitMixin.java
@@ -1,16 +1,45 @@
+
+
 package xyz.trolly.lanlimit.mixin;
 
-import xyz.trolly.lanlimit.LANLimit;
-import net.minecraft.client.gui.screen.TitleScreen;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import net.minecraft.client.gui.screen.TitleScreen;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.integrated.IntegratedServer;
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.world.WorldSaveHandler;
+
+
+@Mixin(PlayerManager.class)
 public class LANLimitMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		LANLimit.LOGGER.info("This line is printed by an example mod mixin!");
-	}
+
+    @ModifyVariable(method = "<init>()V",
+                    at = @At(value = "HEAD"), ordinal = 0)
+    private static int injected(
+                          int maxPlayers) {
+
+        // if (server instanceof IntegratedServer) {
+
+        //     return 10;
+        // }
+        return 10;
+    }
+    // }
+    // @ModifyArg(method =
+    // "<init>(Lnet/minecraft/server/integratedIntegratedServer;Lnet/minecraft/util/registry/DynamicRegistryManager/Impl;Lnet/minecraft/world/WorldSaveHandler;)V",
+    // at = @At(value = "INVOKE",
+    // target =
+    // "Lnet/minecraft/server/PlayerManager;PlayerManager(Lnet/minecraft/server/MinecraftServer;Lnet/minecraft/util/registry/DynamicRegistryManager/Impl;Lnet/minecraft/world/WorldSaveHandler;I)V"),
+    // index = 3)
+    // private int injected(int x) {
+    // return 10;
+    // }
 }
