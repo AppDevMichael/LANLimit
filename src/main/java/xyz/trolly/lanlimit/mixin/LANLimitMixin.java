098f01b3db8464e94070543332602785f0dddc9e
Michael O'Connell <33251702+AppDevMichael@users.noreply.github.com>
2022-01-05 01:31:14 +0000

    Cleanup v2

--- src/main/java/xyz/trolly/lanlimit/mixin/LANLimitMixin.java
+++ src/main/java/xyz/trolly/lanlimit/mixin/LANLimitMixin.java
@@ -2,19 +2,20 @@
 
 package xyz.trolly.lanlimit.mixin;
 
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.At;
-
 import net.minecraft.server.PlayerManager;
 
 
 @Mixin(PlayerManager.class)
 public class LANLimitMixin {
 
-    @ModifyVariable(method = "<init>()V",
-                    at = @At(value = "HEAD"), ordinal = 0)
+    @ModifyVariable(method = "<init>()V", at = @At(value = "HEAD"), ordinal = 0)
     private static int injected(int maxPlayers) {
-        return 10;
+
+        if (maxPlayers == 8) { return 64; }
+        return maxPlayers;
     }
 }
